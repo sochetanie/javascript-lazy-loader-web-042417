1d44b4232ddef5bee525adf3dd7f9511ef6c977c
Alla Gorik <allagorik2901@gmail.com>
2017-06-14 20:33:02 +0000

    Done.

--- spec/01-jasmine/cars-spec.js
+++ spec/01-jasmine/cars-spec.js
@@ -1,7 +1,7 @@
 'use-strict';
 
 describe('Cars', function() {
-  
+
   beforeEach(function(){
     setFixtures('<body><div class="jumbotron"><div class="container"><h1>Car Model Lazy Load</h1></div></div><div class="container"><div id="cars"><div class="row"><div class="col-md-4 car"><h2>Chevrolet</h2><p><strong>Model:</strong> Tahoe</p><p><strong>Year:</strong> 2012</p></div><div class="col-md-4 car"><h2>Toyota</h2><p><strong>Model:</strong> Camry</p><p><strong>Year:</strong> 2002</p></div><div class="col-md-4 car"><h2>Mercedes-Benz</h2><p><strong>Model:</strong> E-Class</p><p><strong>Year:</strong> 1998</p></div></div><div class="row"><div class="col-md-4 car"><h2>GMC</h2><p><strong>Model:</strong> Acadia</p><p><strong>Year:</strong> 2013</p></div><div class="col-md-4 car"><h2>Ford</h2><p><strong>Model:</strong> Fusion</p><p><strong>Year:</strong> 2011</p></div><div class="col-md-4 car"><h2>Chrysler</h2><p><strong>Model:</strong> 300</p><p><strong>Year:</strong> 2005</p></div></div></div><br><div class="row"><div class="col-md-4 col-md-offset-4 center"><button class="btn btn-success" id="load-cars">Load more cars</button></div></div></div></body>');
   });
@@ -12,7 +12,7 @@ describe('Cars', function() {
       var htmlOne = '<div class="row"><div class="col-md-4 car"><h2>Dodge</h2><p><strong>Model:</strong> Avenger</p><p><strong>Year:</strong> 2013</p></div><div class="col-md-4 car"><h2>Nissan</h2><p><strong>Model:</strong> Maxima</p><p><strong>Year:</strong> 2009</p></div><div class="col-md-4 car"><h2>Subaru</h2><p><strong>Model:</strong> Impreza WRX</p><p><strong>Year:</strong> 2013</p></div></div>';
       var resultOne = formatCars(carsOneJSON);
       expect(resultOne).toEqual(htmlOne);
-      
+
       var carsTwoJSON = [{"Make":"Acura","Model":"TL","Year":"2012"},{"Make":"Mercury","Model":"Sable","Year":"2008"},{"Make":"Lexus","Model":"ES 250","Year":"2012"}];
       var htmlTwo = '<div class="row"><div class="col-md-4 car"><h2>Acura</h2><p><strong>Model:</strong> TL</p><p><strong>Year:</strong> 2012</p></div><div class="col-md-4 car"><h2>Mercury</h2><p><strong>Model:</strong> Sable</p><p><strong>Year:</strong> 2008</p></div><div class="col-md-4 car"><h2>Lexus</h2><p><strong>Model:</strong> ES 250</p><p><strong>Year:</strong> 2012</p></div></div>';
       var resultTwo = formatCars(carsTwoJSON);
@@ -28,12 +28,12 @@ describe('Cars', function() {
       addCarsToDOM(carsOneJSON);
       expect($('#cars').children().length).toEqual(rowCount + 1);
       expect($('#cars').children().last().children().length).toEqual(3);
-     
+
       var firstLastCar = $('#cars').children().last().children().last();
       expect(getBrand(firstLastCar)).toEqual("Subaru");
       expect(getModel(firstLastCar)).toEqual("Model: Impreza WRX");
       expect(getYearDate(firstLastCar)).toEqual("Year: 2013");
-      
+
       var carsTwoJSON = [{"Make":"Acura","Model":"TL","Year":"2012"},{"Make":"Mercury","Model":"Sable","Year":"2008"},{"Make":"Lexus","Model":"ES 250","Year":"2012"}];
       addCarsToDOM(carsTwoJSON);
       expect($('#cars').children().length).toEqual(rowCount + 2);
